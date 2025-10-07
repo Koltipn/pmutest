3d01312bb6b8c0867da3803974490936430fba4f
Kolti <mr.droidcaxa@mail.ru>
2025-10-07 18:07:20 +0700

    Make game react to settings changes

--- lab3/app/src/main/java/com/example/androidgamekt/util/ViewPagerAdapter.kt
+++ lab3/app/src/main/java/com/example/androidgamekt/util/ViewPagerAdapter.kt
@@ -4,20 +4,22 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.androidgamekt.model.AuthorsFragment
+import com.example.androidgamekt.model.GameFragment
 import com.example.androidgamekt.model.RulesFragment
 import com.example.androidgamekt.model.SettingsFragment
 import com.example.androidgamekt.model.SignUpFragment
 
 class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
-    override fun getItemCount(): Int = 4
+    override fun getItemCount(): Int = 5
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> SignUpFragment()
-            1 -> RulesFragment()
-            2 -> AuthorsFragment()
-            3 -> SettingsFragment()
-            else -> SignUpFragment()
+            0 -> GameFragment()
+            1 -> SignUpFragment()
+            2 -> RulesFragment()
+            3 -> AuthorsFragment()
+            4 -> SettingsFragment()
+            else -> GameFragment()
         }
     }
 }
